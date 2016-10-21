97d0b24cebbdf82e2bd68b2038dcf100316a0e2d
Chris Fuller <chris@chrisandweeze.com>
2016-10-21 17:39:24 -0400

    improving player animation

--- js/fox.js
+++ js/fox.js
@@ -1,27 +1,32 @@
 $(document).ready(function() {
-  $('.btn-enlarge').click(function() {
-    var w = $('.container').width();
-    if ($(this).hasClass('big')) {
-      $('#main').animate({
-          width: w/2},
-        1000,
-        function() {
-          $('.meta').show();
-          $('#main').removeClass('col-sm-12').addClass('col-sm-6');
-        }
-      );
-      $(this).removeClass('big').text('Enlarge');
-    }
-    else {
-      $('.meta').hide();
-      $('#main').animate({
-        width: w},
-        1000,
-        function() {
-          $('#main').removeClass('col-sm-6').addClass('col-sm-12');
-        }
-      );
-      $(this).addClass('big').text('Reduce');
-    }
+  $(document).on('playerLoaded', function() {
+
+    $('#main').append('<a href="#" class="btn-enlarge">Enlarge</a>');
+
+    $('.btn-enlarge').click(function() {
+      var w = $('.container').width();
+      if ($(this).hasClass('big')) {
+        $('#main').animate({
+            width: w/2},
+          1000,
+          function() {
+            $('.meta').show();
+            $('#main').removeClass('col-sm-12').addClass('col-sm-6');
+          }
+        );
+        $(this).removeClass('big').text('Enlarge');
+      }
+      else {
+        $('.meta').hide();
+        $('#main').animate({
+            width: w},
+          1000,
+          function() {
+            $('#main').removeClass('col-sm-6').addClass('col-sm-12');
+          }
+        );
+        $(this).addClass('big').text('Reduce');
+      }
+    });
   });
 });

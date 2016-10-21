86a24fe45ce52836f21d73939c72052b2cabc396
Chris Fuller <chris@chrisandweeze.com>
2016-10-21 09:16:40 -0400

    adding player animation

---
+++ js/fox.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('.btn-enlarge').click(function() {
+    var w = $('.container').width();
+    if ($(this).hasClass('big')) {
+      $('#main').animate({
+          width: w/2},
+        1000,
+        function() {
+          $('.meta').show();
+          $('#main').removeClass('col-sm-12').addClass('col-sm-6');
+        }
+      );
+      $(this).removeClass('big').text('Enlarge');
+    }
+    else {
+      $('.meta').hide();
+      $('#main').animate({
+        width: w},
+        1000,
+        function() {
+          $('#main').removeClass('col-sm-6').addClass('col-sm-12');
+        }
+      );
+      $(this).addClass('big').text('Reduce');
+    }
+  });
+});

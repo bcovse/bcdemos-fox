952c1bf89e9bb0670a4aa538c12c29594b1ae99d
Chris Fuller <chris@chrisandweeze.com>
2016-10-28 17:23:49 -0400

    first pass at auto advance with custom end screen

--- js/upNext.js
+++ js/upNext.js
@@ -11,15 +11,26 @@ videojs.plugin('upNext', function() {
     var player = this;
     var playlist = player.playlist();
     var current = player.playlist.currentItem();
-    var next = current + 1;
+    var next = playlist[current + 1];
+    var delay = 5;
 
-    var overlayTemplate = '<div class="lock-overlay">{{content}}</div>';
-
-    console.log(playlist);
-
-    player.playlist.autoadvance(5);
+    player.playlist.autoadvance(delay);
     player.on('ended', function() {
+      var content = '<div class="counter-overlay" style="background-image: url(' + next.poster + ')">' +
+      '<span class="timer">Next video in <span class="clock">' + delay + '</span> seconds</span></div>';
       overlay(player, content);
+      var i = 1;
+      var countdown = setInterval(function() {
+        $('.clock').html(delay - i);
+        i++;
+        if(i == delay){
+          clearInterval(countdown);
+          i = 1;
+        }
+      }, 1000);
+    });
+    player.on('loadstart', function() {
+      $('.counter-overlay').remove();
     });
   });
 });
